01a969d9ad9a9286070cc67f4b538164f67bd88a
yangzi6666 <784144498@qq.com>
2017-10-16 18:41:53 +0800

    自动升级完成了

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class VersionEntity {
+    public String versionCode;
+
+    public String description;
+
+    public String apkurl;
+}
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
@@ -17,6 +17,7 @@ public class MyUtils {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
             return "";
+
         }
     }
 }
