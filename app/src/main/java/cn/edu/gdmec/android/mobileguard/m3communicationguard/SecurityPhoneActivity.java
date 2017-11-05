e62ebf43f3bfbd748aec6671ca98274772b94fe2
yangzi6666 <784144498@qq.com>
2017-11-06 00:27:45 +0800

    通讯卫士

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/SecurityPhoneActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/SecurityPhoneActivity.java
@@ -100,7 +100,7 @@ public class SecurityPhoneActivity extends AppCompatActivity implements View.OnC
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_security_phone);
         initView();
         fillData();
