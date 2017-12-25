b979f17a56536f3e9b85be88f53a5aecfe6765d1
yangzi6666 <784144498@qq.com>
2017-12-25 20:45:50 +0800

    流量管理2

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AdvancedToolsActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m9advancedtools;
+
+/**
+ * Created by Administrator on 2017/12/25.
+ */
+
+public class AdvancedToolsActivity {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/dao/NumBelongtoDao.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m9advancedtools.db.dao;
+
+/**
+ * Created by Administrator on 2017/12/25.
+ */
+
+public class NumBelongtoDao {
+}
