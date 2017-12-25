a5588822739b6911310d329d191ff552b581bc6b
yangzi6666 <784144498@qq.com>
2017-12-25 19:16:31 +0800

    流量管理

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -29,6 +29,7 @@ import cn.edu.gdmec.android.mobileguard.m4appmanager.AppManagerActivity;
 import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanActivity;
 import cn.edu.gdmec.android.mobileguard.m6cleancache.CacheClearListActivity;
 import cn.edu.gdmec.android.mobileguard.m6cleancache.CleanCacheActivity;
+import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.TrafficMonitoringActivity;
 
 public class HomeActivity extends AppCompatActivity {
     private GridView gv_home;
@@ -72,6 +73,9 @@ private SharedPreferences msharedPreferences;
                         break;
                     case 4:
                       startActivity(CacheClearListActivity.class);
+                        break;
+                    case 6:
+                        startActivity(TrafficMonitoringActivity.class);
                         break;
                 }
 
